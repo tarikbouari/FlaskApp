34d1ada73bbc6e893bce64f4aa605e3ea42a699f
tarikbouari <tarikbouari@gmail.com>
2025-12-11 14:28:21 +0100

    Mis a jour du fichier CI

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
  return 'Hello, World!'
+@app.route('/health')
+def check_health():
+ return "bienvenu sur votre espace santé"
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=5000, debug=True)
