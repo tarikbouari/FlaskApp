552a42a9a9b924e90b9f0029c40a9c7b495565ac
tarikbouari <tarikbouari@gmail.com>
2025-12-12 15:41:35 +0100

    Resolution d'erreur

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_index():
     tester = app.test_client() 
     response = tester.get('/') 
     assert response.status_code == 200 
-    assert b"Hello, World!" in response.data 
+    assert b"Hello, World! Welcome to my app" in response.data 
